86f287c38d2350cd2014cfc706282d3d376e21ce
zain-ali512 <zain.ali@graana.com>
2024-05-07 11:55:41 +0500

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-import { Lato } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
-import Image from "next/image";
-import background from "../../public/background.jpg";
 import { Meteors } from "@/components/ui/Meteors";
 
-const lato = Lato({ subsets: ["latin"], weight: ["100", "300", "400", "700", "900"] });
+const lato = Raleway({ subsets: ["latin"], weight: ["100", "300", "400", "700", "900"] });
 
 export const metadata: Metadata = {
   title: "Realtime Chat App",
@@ -20,15 +18,16 @@ const RootLayout = ({
   return (
     <html lang="en">
       <body className={lato.className}>
-        <div className="relative w-full">
+        <div className="relative">
           <div className="absolute w-screen overflow-hidden h-screen">
             <Meteors number={20} />
           </div>
-          <div className="absolute -z-10 w-full">
-            <Image src={background} alt="Background Image" className="w-screen h-screen object-cover" priority></Image>
-          </div>
         </div>
-        <div>{children}</div>
+        <div className="absolute -left-40 top-1/2 -translate-y-1/2 transform w-96 h-full bg-[#30d5c8] rounded-full blur-3xl bg-opacity-10"></div>
+        <div className="absolute -right-40 top-1/2 -translate-y-1/2 transform w-96 h-full bg-[#ff007f] rounded-full blur-3xl bg-opacity-10"></div>
+        <div className="absolute -top-40 left-1/2 transform -translate-x-1/2 w-full h-96 bg-[#a020f0] rounded-full blur-3xl bg-opacity-10"></div>
+        <div className="absolute -bottom-40 left-1/2 transform -translate-x-1/2 w-full h-96 bg-[#16a34a] rounded-full blur-3xl bg-opacity-10"></div>
+        <div className="size-full">{children}</div>
       </body>
     </html>
   );
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,32 +1,28 @@
 "use client";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3dCard";
-import Image from "next/image";
-import Link from "next/link";
-import img from "../../../public/image.jpg";
 
 const page = () => {
   return (
-    <CardContainer className="">
-      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-white/50 backdrop-blur-md shadow-lg dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
-        <CardItem translateZ="50" className="text-xl font-bold text-neutral-600 dark:text-white">
-          Welcome to the Chat App
-        </CardItem>
-        <CardItem as="p" translateZ="60" className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
-          Hover over this card to unleash the power of CSS perspective
-        </CardItem>
-        <CardItem translateZ="100" className="w-full mt-4">
-          <Image src={img} height="1000" width="1000" className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl" alt="thumbnail" />
-        </CardItem>
-        <div className="flex justify-between items-center mt-20">
-          <CardItem translateZ={20} as={Link} href="" target="__blank" className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white">
-            Try now →
+    <div className="h-full flex justify-center items-center">
+      <CardContainer>
+        <CardBody className="group/card hover:shadow-[0_0_15px_rgba(57,255,20,0.8)] bg-gray-800 backdrop-blur-sm shadow-lg bg-opacity-20 h-auto rounded-xl p-6 border space-y-4">
+          <CardItem translateZ="50" className="text-xl font-bold">
+            Welcome to the Chat App
           </CardItem>
-          <CardItem translateZ={20} as="button" className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
-            Sign up
+          <CardItem translateZ="100" className="w-full">
+            <input type="text" placeholder="Enter Username / Number / Email" autoComplete="new-username" className="w-full bg-black px-4 py-2 rounded-md" />
           </CardItem>
-        </div>
-      </CardBody>
-    </CardContainer>
+          <CardItem translateZ="100" className="w-full">
+            <input type="password" placeholder="Enter Password" autoComplete="new-password" className="w-full bg-black px-4 py-2 rounded-md" />
+          </CardItem>
+          <div className="flex w-full justify-end">
+            <CardItem translateZ={20} as="button" className="px-4 py-2 rounded-md bg-[#39FF14] text-sm font-bold hover:bg-[#37ff14c7]">
+              Login
+            </CardItem>
+          </div>
+        </CardBody>
+      </CardContainer>
+    </div>
   );
 };
 export default page;
